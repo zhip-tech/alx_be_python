1cebe85c42f66831237df17271f4617facd0f64e
Ogochukwu Ike <ogochike508@gmail.com>
2025-11-09 11:53:11 +0100

    simple interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,9 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> principal = 1000
+>>> rate = 0.05
+>>> time = 3
+>>> 
+>>> I = principal * rate * time
+>>> print(f"The simple interest is: {I}")
+The simple interest is: 150.0
