348d18eda6f1300916b84554e33d81eaaaf32815
Ogochukwu Ike <ogochike508@gmail.com>
2025-11-09 12:01:28 +0100

    new change

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,9 +1,8 @@
-Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
->>> principal = 1000
->>> rate = 0.05
->>> time = 3
->>> 
->>> I = principal * rate * time
->>> print(f"The simple interest is: {I}")
+
+P = 1000
+R = 0.05
+T = 3
+
+I = P * R * T
+print(f"The simple interest is: {I}")
 The simple interest is: 150.0
