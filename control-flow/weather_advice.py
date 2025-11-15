df743d49126ab0de1576faceebb93fb5141ab49c
Ogochukwu Ike <ogochike508@gmail.com>
2025-11-15 20:55:24 +0100

    another correction

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,13 +1,13 @@
 question = input("What's the weather like today? (sunny/rainy/cold): ")
 
 if question == "sunny":
-    print("Wear a t-shirt and sunglasses")
+    print("Wear a t-shirt and sunglasses.")
 
 elif question == "rainy":
-    print("Don't forget your umbrella and a raincoat")
+    print("Don't forget your umbrella and a raincoat.")
 
 elif question == "cold":
-    print("Make sure to wear a warm coat and a scarf")
+    print("Make sure to wear a warm coat and a scarf.")
 
 else:
-    print("Sorry, I don't have recommendations for this weather")
+    print("Sorry, I don't have recommendations for this weather.")
