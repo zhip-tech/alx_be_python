e7e9d92e6a2ee68b247c120f0edd3b01773bf586
Ogochukwu Ike <ogochike508@gmail.com>
2025-11-15 21:21:00 +0100

    new com

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,10 @@
+number = int (input("Enter the number to see the multiplication table: "))
+
+
+for i in range(1, 11):
+    
+    product = number * i
+    
+    print(f"{number} * {i} = {product}")
+        
+    
