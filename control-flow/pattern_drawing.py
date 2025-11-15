fdc51f73567967dd3917e42c8386fde11198d7ab
Ogochukwu Ike <ogochike508@gmail.com>
2025-11-15 21:25:11 +0100

    new one

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,10 @@
+# pattern_drawing.py
+
+size = int(input("Enter the size of the pattern: "))
+
+row = 0
+while row < size:
+    for col in range(size):
+        print("*", end="")
+    print()  # Move to the next line
+    row += 1
