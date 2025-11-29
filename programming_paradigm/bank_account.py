56a0d145b40a436403240019a0e5738b423fad21
Ogochukwu Ike <ogochike508@gmail.com>
2025-11-29 10:16:03 +0100

    project1

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,27 @@
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        # Encapsulated attribute
+        self.__account_balance = initial_balance
+
+    def deposit(self, amount):
+        """Add amount to balance"""
+        if amount > 0:
+            self.__account_balance += amount
+        else:
+            print("Deposit amount must be positive.")
+
+    def withdraw(self, amount):
+        """Withdraw amount if sufficient funds"""
+        if amount <= 0:
+            print("Withdrawal amount must be positive.")
+            return False
+
+        if self.__account_balance >= amount:
+            self.__account_balance -= amount
+            return True
+        else:
+            return False
+
+    def display_balance(self):
+        """Print the current account balance"""
+        print(f"Current Balance: ${self.__account_balance}")
