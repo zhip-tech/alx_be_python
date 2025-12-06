e433400d4114399e6a86f005d1fbcfb6a0b6756d
Ogochukwu Ike <ogochike508@gmail.com>
2025-12-06 10:36:10 +0100

    hoo

--- oop/library_system.py
+++ oop/library_system.py
@@ -4,30 +4,30 @@ class Book:
         self.author = author
 
     def __str__(self):
-        return f"{self.title} by {self.author}"
+        return f"Book: {self.title} by {self.author}"
 
 
 class EBook(Book):
     def __init__(self, title, author, file_size):
-        super().__init__(title, author)   # Call the base class constructor
+        super().__init__(title, author)
         self.file_size = file_size
 
     def __str__(self):
-        return f"{self.title} by {self.author} - EBook, File Size: {self.file_size}MB"
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
-        super().__init__(title, author)   # Call the base class constructor
+        super().__init__(title, author)
         self.page_count = page_count
 
     def __str__(self):
-        return f"{self.title} by {self.author} - PrintBook, Pages: {self.page_count}"
-
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
+        
 
 class Library:
     def __init__(self):
-        self.books = []  # Composition: A library "has" books
+        self.books = []
 
     def add_book(self, book):
         self.books.append(book)
