8320802cc008690d6e092257cfebec90462c11b4
Ogochukwu Ike <ogochike508@gmail.com>
2025-12-07 21:01:32 +0100

    bnno

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,24 @@
+import math
+
+
+class Shape:
+    def area(self):
+        # This method must be overridden by subclasses
+        raise NotImplementedError("Subclasses must implement this method")
+
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * (self.radius ** 2)
